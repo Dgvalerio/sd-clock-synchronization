36e233286ad85d3eaef99b4af1ae485fbf597d48
davigvalerio <al.davigvalerio@gmail.com>
2024-03-14 13:11:55 -0300

    feat(project): final files

--- client.py
+++ client.py
@@ -1,28 +1,44 @@
+import datetime
+import time
 import grpc
+
+import sys
 import clock_pb2
 import clock_pb2_grpc
 
 
-def get_time(stub):
-    response = stub.GetTime(clock_pb2.TimeRequest(client_time=int(time.time() * 1000)))
-    return response.server_time
+def get_time_offset(stub, client_id):
+    local_time = time.time()
+
+    print(sys.argv, len(sys.argv), len(sys.argv) == 3)
+
+    if len(sys.argv) == 3:
+        print(sys.argv[2])
+        local_time += int(sys.argv[2])
+
+    request = clock_pb2.TimeOffsetRequest(timestamp=local_time, client_id=client_id)
+    response = stub.GetTimeOffset(request)
+    return response.offset
 
 
-def adjust_time(stub, client_time):
-    response = stub.AdjustTime(clock_pb2.AdjustRequest(client_time=client_time))
-    if response.success:
-        print("Time adjusted successfully.")
-    else:
-        print("Failed to adjust time.")
+def get_synchronized_time(stub, client_id):
+    request = clock_pb2.SynchronizedTimeRequest(client_id=client_id)
+    response = stub.GetSynchronizedTime(request)
+    return response.time
 
 
-def run():
+def run_client(client_id):
     channel = grpc.insecure_channel('localhost:50051')
-    stub = clock_pb2_grpc.ClockSyncStub(channel)
+    stub = clock_pb2_grpc.ClockServiceStub(channel)
+    offset = get_time_offset(stub, client_id)
 
-    client_time = get_time(stub)
-    adjust_time(stub, client_time)
+    while True:
+        local_time = datetime.datetime.fromtimestamp(get_synchronized_time(stub, client_id))
+        synchronized_time = datetime.datetime.fromtimestamp(get_synchronized_time(stub, client_id) - offset)
+        print(f"[{client_id}] Sincronizado: {synchronized_time} | Local: {local_time}")
+        time.sleep(1)
 
 
 if __name__ == '__main__':
-    run()
+    print(sys.argv[1])
+    run_client(sys.argv[1])
--- server.py
+++ server.py
@@ -1,36 +1,40 @@
+import datetime
 import time
 from concurrent import futures
+
 import grpc
+
 import clock_pb2
 import clock_pb2_grpc
 
-_ONE_DAY_IN_SECONDS = 60 * 60 * 24
 
-
-class ClockSyncServicer(clock_pb2_grpc.ClockSyncServicer):
+class ClockService(clock_pb2_grpc.ClockServiceServicer):
     def __init__(self):
-        self.client_times = []
+        self.offsets = {}
 
-    def GetTime(self, request, context):
-        return clock_pb2.TimeResponse(server_time=int(time.time() * 1000))
+    def GetTimeOffset(self, request, context):
+        client_time = request.timestamp
+        server_time = time.time()
+        offset = (server_time - client_time) / 2
+        self.offsets[request.client_id] = offset
+        return clock_pb2.TimeOffsetResponse(offset=offset)
 
-    def AdjustTime(self, request, context):
-        avg_time = sum(self.client_times) // len(self.client_times)
-        adjustment = avg_time - request.client_time
-        return clock_pb2.AdjustResponse(success=True)
+    def GetSynchronizedTime(self, request, context):
+        if request.client_id not in self.offsets:
+            return clock_pb2.SynchronizedTimeResponse(time=0)
+
+        offset = self.offsets[request.client_id]
+        synchronized_time = time.time() + offset
+        print(f"[{request.client_id}] {datetime.datetime.fromtimestamp(synchronized_time)} | (Tempo: {synchronized_time}, diferença: {offset})")
+        return clock_pb2.SynchronizedTimeResponse(time=synchronized_time)
 
 
 def serve():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
-    clock_pb2_grpc.add_ClockSyncServicer_to_server(ClockSyncServicer(), server)
+    clock_pb2_grpc.add_ClockServiceServicer_to_server(ClockService(), server)
     server.add_insecure_port('[::]:50051')
     server.start()
-    print("Server started!")
-    try:
-        while True:
-            time.sleep(_ONE_DAY_IN_SECONDS)
-    except KeyboardInterrupt:
-        server.stop(0)
+    server.wait_for_termination()
 
 
 if __name__ == '__main__':
